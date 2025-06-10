57abc64a5cb98110f1567f0f84de2f4d60bfe5c6
Pablodomcha <pablohillnor@gmail.com>
2025-06-10 23:50:36 +0200

    Milestone and Structure had content swapped

--- Randomizer/randomrecipes/src/main/java/com/satisfactoryrandomizer/Storage/Randomizables/Milestone.java
+++ Randomizer/randomrecipes/src/main/java/com/satisfactoryrandomizer/Storage/Randomizables/Milestone.java
@@ -1,8 +1,22 @@
 package com.satisfactoryrandomizer.Storage.Randomizables;
 
+import java.util.List;
+
 public class Milestone extends Randomizable {
+
+    private String rewards;
+    // Recipepath is a shcematic path, but W/e
+
     // Basic constructor
-    public Milestone(String name, Boolean available, Boolean craftable, String recipepath, String extraCheck) {
-        super(name, recipepath + "." + recipepath + "_C", available, craftable, null);
+    public Milestone(String name, Boolean available, Boolean craftable, String recipepath, List<String> extraCheck) {
+        super(name, recipepath + "." + recipepath + "_C", available, craftable, extraCheck);
+    }
+    
+    public String getRewards() {
+        return this.rewards;
+    }
+
+    public void setRewards(String rewards) {
+        this.rewards = rewards;
     }
 }
