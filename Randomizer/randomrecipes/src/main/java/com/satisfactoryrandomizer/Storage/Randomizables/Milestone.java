f488cc51b5bcc6a33d532d9c92e5df24983d2388
Pablodomcha <pablohillnor@gmail.com>
2025-06-11 01:24:14 +0200

    made rewads have 2 fields

--- Randomizer/randomrecipes/src/main/java/com/satisfactoryrandomizer/Storage/Randomizables/Milestone.java
+++ Randomizer/randomrecipes/src/main/java/com/satisfactoryrandomizer/Storage/Randomizables/Milestone.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Milestone extends Randomizable {
 
-    private List<String> rewards;
+    private List<String[]> rewards;
     // Recipepath is a shcematic path, but W/e
 
     // Basic constructor
@@ -12,17 +12,33 @@ public class Milestone extends Randomizable {
         super(name, recipepath + "." + recipepath + "_C", available, craftable, extraCheck);
     }
 
-    public List<String> getRewards() {
+    /**
+     * Retrieves the list of rewards associated with this milestone.
+     *
+     * @return A list of rewards, where each reward is an array {rewardType, reward}.
+     */
+
+    public List<String[]> getRewards() {
         return this.rewards;
     }
 
-    public String getrmRewards() {
-        String temp = this.rewards.get(0);
+    /**
+     * Retrieves and removes the first reward in the list of rewards associated with this milestone.
+     * 
+     * @return A string array {rewardType, reward}.
+     */
+    public String[] getrmRewards() {
+        String[] temp = this.rewards.get(0);
         this.rewards.remove(0);
         return temp;
     }
 
-    public void setRewards(List<String> rewards) {
+    /**
+     * Sets the rewards for this milestone.
+     * 
+     * @param rewards A list of rewards, where each reward is an array {rewardType, reward}.
+     */
+    public void setRewards(List<String[]> rewards) {
         this.rewards = rewards;
     }
 }
